431421b291326a23b09c037319150b519943bafa
pedrohbarros <64796873+pedrohbarros@users.noreply.github.com>
2022-05-18 15:35:23 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,51 @@
+// pra executar alguma coisa no js tem que usar ()*
+//STRing(TEXTO)
+//Number (numeros)
+//Bollean (true or false)
+
+window.addEventListener('scroll', onScroll)
+onScroll()
+function onScroll() {
+  showNavOnScroll()
+  showBackToTopButtonOnScroll()
+}
+
+function showNavOnScroll (){
+  if (scrollY > 100) {
+    navigation.classList.add('scroll')
+  } else {
+  navigation.classList.remove('scroll')
+ }
+}
+
+function showBackToTopButtonOnScroll(){
+  if (scrollY > 400) {
+    backToTopButton.classList.add('show')
+  } else {
+  backToTopButton.classList.remove('show')
+ }
+}
+
+function openMenu(){
+ document.body.classList.add('menu-expanded')
+}
+
+function closeMenu(){
+ document.body.classList.remove('menu-expanded')
+}
+ScrollReveal({
+  origin:'top',
+  distance: '30px',
+  duration:700,
+}).reveal(`
+#home, 
+#home img, 
+#home .stats, 
+#services,
+#services header,
+#services .card,
+#about,
+#about header,
+#about .content`);
+
+
